fc7ff6fabfa526c9a3057270784184eaa6552cd5
Adrián Araque <106025865+adrarom@users.noreply.github.com>
2023-12-18 15:39:14 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  " when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+function GenerateRandomExcuse() {
+  let finalExcuse = "";
+  finalExcuse += who[SetRandomNumber(who)] + " ";
+  finalExcuse += action[SetRandomNumber(action) + " "];
+  finalExcuse += what[SetRandomNumber(what) + " "];
+  finalExcuse += when[SetRandomNumber(when)];
+  return finalExcuse;
+}
+console.log(GenerateRandomExcuse());
+function SetRandomNumber(anArray) {
+  return (Math.random() * 10) % anArray.length;
+}
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
